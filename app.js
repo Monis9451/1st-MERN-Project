2335cb1edced8117f1a4c2fa4e1c7daeca74fb0b
Monis9451 <mianmonishussain786@gmail.com>
2024-08-21 19:33:28 +0500

    add index and show info routes

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/listing.js")
+const path = require("path")
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
 
 async function main(){
     await mongoose.connect(MONGO_URL);
@@ -14,13 +18,22 @@ app.listen(port, ()=>{
     console.log("Port 8080 is listening");
 });
 
+//Root
 app.get("/", (req, res)=>{
     res.send("Hey, I'm root!");
 });
 
+//Index route
 app.get("/listing", async(req, res)=>{
-    let allListings = await Listing.find().then((res)=>{console.log(res)});
-    res.render(index.ejs, {allListings})
+    let allListings = await Listing.find();
+    res.render("listing/index", {allListings})
+})
+
+//Show route
+app.get("/listing/:id", async(req, res)=>{
+    let {id} = req.params;
+    const listData = await Listing.findById(id);
+    res.render("listing/show",{listData})
 })
 
 // app.get("/testListing", async(req, res)=>{
