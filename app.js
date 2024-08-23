ede2e7ce3e16130690908614afcb54c4e542ad33
Monis9451 <mianmonishussain786@gmail.com>
2024-08-23 06:10:43 +0500

    Add an edit route

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const port = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("./models/listing.js")
 const path = require("path")
+const methodOverride = require("method-override")
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended:true}));
 
 async function main(){
@@ -44,20 +46,28 @@ app.get("/listing/:id", async(req, res)=>{
 //Create route
 app.post("/listing", async(req, res)=>{
     await Listing.create(req.body.listing);
-    res.redirect("/listing")
+    res.redirect("/listing");
 })
 
 //Edit route
 app.get("/listing/:id/edit", async(req, res)=>{
-    let {id} = req.params
-    let data = await Listing.findById(id)
-    res.render("listing/edit", {data})
+    let {id} = req.params;
+    let data = await Listing.findById(id);
+    res.render("listing/edit", {data});
 })
 
 //Save editing route
-app.post("/listing/:id", async(req, res)=>{
-    let newData = req.body.listing
-    console.log(newData)
+app.patch("/listing/:id", async(req, res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, req.body.listing);
+    res.redirect(`/listing/${id}`);
+})
+
+//Delete route
+app.delete("/listing/:id", async(req, res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listing");
 })
 
 // app.get("/testListing", async(req, res)=>{
@@ -70,4 +80,4 @@ app.post("/listing/:id", async(req, res)=>{
 //     })
 //     .then((res)=>{console.log(res)})
 //     res.send("Testing successfull")
-// });
+// });'
