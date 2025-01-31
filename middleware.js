ca665f2d9729f6242a0055cd3b4a1ff1407de4e5
Monis9451 <mianmonishussain786@gmail.com>
2025-02-01 02:43:14 +0500

    Listing authorized

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const Listing = require("./models/listing");
+const ExpressError = require("./utils/ExpressError.js")
+const {listingSchema, reviewSchema} = require("./schema.js")
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.redirectURL = req.originalUrl;
@@ -12,4 +16,34 @@ module.exports.saveRedirectURL = (req, res, next) => {
         res.locals.redirectURL = req.session.redirectURL;
     }
     next();
+}
+
+module.exports.isOwner = async(req, res, next) => {
+        let {id} = req.params;
+        let listing = await Listing.findById(id)
+        if(!listing.owner.equals(res.locals.currentUser._id)){
+            req.flash("error", "You do not have permission to update this listing");
+            res.redirect(`/listing/${id}`);
+    }
+    next();
+}
+
+module.exports.validateListing = (req, res, next)=>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+}
+
+module.exports.validateReview = (req, res, next)=>{
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
 }
--- routes/listing.js
+++ routes/listing.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js")
-const ExpressError = require("../utils/ExpressError.js")
-const {listingSchema, reviewSchema} = require("../schema.js")
 const Listing = require("../models/listing.js")
-const {isLoggedIn} = require("../middleware.js")
-
-const validateListing = (req, res, next)=>{
-    let {error} = listingSchema.validate(req.body);
-    if(error){
-        let errMsg = error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }else{
-        next();
-    }
-}
+const {isLoggedIn, isOwner, validateListing} = require("../middleware.js")
 
 //Index route
 router.get("/", wrapAsync(async(req, res)=>{
@@ -49,7 +37,7 @@ router.post("/", validateListing, isLoggedIn, wrapAsync(async(req, res, next)=>{
 }))
 
 //Edit route
-router.get("/:id/edit", isLoggedIn, wrapAsync(async(req, res)=>{
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(async(req, res)=>{
 let {id} = req.params;
 let data = await Listing.findById(id);
 if(!data){
@@ -60,7 +48,7 @@ res.render("listing/edit", {data});
 }))
 
 //Update route
-router.patch("/:id", isLoggedIn, wrapAsync(async(req, res)=>{
+router.patch("/:id", isLoggedIn, isOwner, wrapAsync(async(req, res)=>{
 let {id} = req.params;
 await Listing.findByIdAndUpdate(id, req.body.listing);
 req.flash("success", "Successfully updated the existing listing");
@@ -68,7 +56,7 @@ res.redirect(`/listing/${id}`);
 }))
 
 //Delete route
-router.delete("/:id", isLoggedIn, wrapAsync(async(req, res)=>{
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync(async(req, res)=>{
 let {id} = req.params;
 await Listing.findByIdAndDelete(id);
 req.flash("success", "Successfully Deleted an existing listing");
